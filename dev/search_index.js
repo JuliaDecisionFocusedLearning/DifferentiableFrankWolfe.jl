9d006150347110b0eadad4801e7511c66336f9fe
Documenter.jl <documenter@juliadocs.github.io>
2023-04-27 07:36:21 +0000

    build based on 150c0be

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = DifferentiableFrankWolfe","category":"page"},{"location":"#DifferentiableFrankWolfe","page":"Home","title":"DifferentiableFrankWolfe","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for DifferentiableFrankWolfe.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [DifferentiableFrankWolfe]","category":"page"}]
+}
