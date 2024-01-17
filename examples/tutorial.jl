2b90177d3bd35aa66df148a495834844b8e93ad6
Guillaume Dalle <22795598+gdalle@users.noreply.github.com>
2024-01-17 15:37:39 +0100

    Update (#12)
    * CompatHelper: bump compat for FrankWolfe to 0.3, (keep existing compat)
    
    * Update
    
    * Fix compat
    
    ---------
    
    Co-authored-by: CompatHelper Julia <compathelper_noreply@julialang.org>

--- examples/tutorial.jl
+++ examples/tutorial.jl
@@ -18,9 +18,14 @@ dfw = DiffFW(f, f_grad1, lmo);  # ... is equivalent to a simplex projection
 # Calling the wrapper
 
 θ = rand(10)
-frank_wolfe_kwargs = (max_iteration=100, epsilon=1e-4)
 
+#-
+
+frank_wolfe_kwargs = (max_iteration=100, epsilon=1e-4)
 y = dfw(θ; frank_wolfe_kwargs)
+
+#-
+
 y_true = simplex_projection(θ)
 @test Vector(y) ≈ Vector(y_true) atol = 1e-3
 
@@ -29,6 +34,8 @@ y_true = simplex_projection(θ)
 J1 = Zygote.jacobian(_θ -> dfw(_θ; frank_wolfe_kwargs), θ)[1]
 J1_true = Zygote.jacobian(simplex_projection, θ)[1]
 @test J1 ≈ J1_true atol = 1e-3
+
+#-
 
 J2 = ForwardDiff.jacobian(_θ -> dfw(_θ; frank_wolfe_kwargs), θ)
 J2_true = ForwardDiff.jacobian(simplex_projection, θ)
