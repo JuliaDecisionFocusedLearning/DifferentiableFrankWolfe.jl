850b929582276128fb083028eae06c3042d8f405
Guillaume Dalle <22795598+gdalle@users.noreply.github.com>
2025-06-10 23:24:03 +0200

    Add projection correctness test

--- test/correctness.jl
+++ test/correctness.jl
@@ -13,6 +13,22 @@
     @test dfw2.implicit.linear_solver == \
 end
 
+@testitem "Projection" begin
+    using ProximalOperators
+    using DifferentiableFrankWolfe: simplex_projection
+    using Test
+    using Zygote
+
+    for n in (2, 5, 10, 50)
+        for _ in 1:10
+            x = rand(n)
+            @test simplex_projection(x) ≈ prox(IndSimplex(1.0), x)[1]
+            @test Zygote.jacobian(simplex_projection, x)[1] ≈
+                ForwardDiff.jacobian(simplex_projection, x)
+        end
+    end
+end
+
 @testitem "Tutorial" begin
     include(joinpath(@__DIR__, "..", "examples", "tutorial.jl"))
 end
