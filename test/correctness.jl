5f97ad86c73c6ad55ca441b570fe2898c1811652
Guillaume Dalle <22795598+gdalle@users.noreply.github.com>
2026-06-26 12:07:11 +0200

    fix: make compatible with matrices

--- test/correctness.jl
+++ test/correctness.jl
@@ -9,7 +9,7 @@
     dfw1 = DiffFW(f, f_grad1, lmo)
     @test !isa(dfw1.implicit.linear_solver, DirectLinearSolver)
 
-    implicit_kwargs = (; linear_solver = DirectLinearSolver())
+    implicit_kwargs = (; linear_solver=DirectLinearSolver())
     dfw2 = DiffFW(f, f_grad1, lmo; implicit_kwargs)
     @test dfw2.implicit.linear_solver isa DirectLinearSolver
 end
@@ -37,7 +37,7 @@ end
     using Test
     using Zygote
 
-    fwkw = (; max_iteration = 1000, epsilon = 1.0e-4, line_search = FrankWolfe.Agnostic())
+    fwkw = (; max_iteration=1000, epsilon=1.0e-4, line_search=FrankWolfe.Agnostic())
 
     @testset "Simplex projection" begin
         lmo = FrankWolfe.ProbabilitySimplexLMO(1.0)
@@ -45,7 +45,8 @@ end
 
         for n in (2, 5, 10), scaling in (0.1, 1, 10)
             θ = scaling .* rand(StableRNG(n), n)  # outside of the simplex a.s.
-            x0 = ones(n) ./ n
+            x0 = ones(3, n)
+            x0 ./= length(x0)
             @test dfw(θ, x0; fwkw...) ≈ true_simplex_projection(θ) rtol = 1.0e-1
             J = Zygote.jacobian(_θ -> dfw(_θ, x0; fwkw...), θ)[1]
             J_true = ForwardDiff.jacobian(true_simplex_projection, θ)
@@ -73,10 +74,10 @@ end
         f(x, θ) = 0.5 * sum(abs2, x .- sqrt.(vcat(θ, sum(θ))))
         f_grad1(x, θ) = x .^ 2 .- vcat(θ, sum(θ))
         lmo = FrankWolfe.BoxLMO(zeros(3), ones(3))
-        dfw = DiffFW(f, f_grad1, lmo; step_size = 1)
+        dfw = DiffFW(f, f_grad1, lmo; step_size=1)
         θ = [0.3, 0.2]
         x0 = [0.7, 0.5, 0.9]
-        fwkw = (; max_iteration = 1000, epsilon = 1.0e-4)
+        fwkw = (; max_iteration=1000, epsilon=1.0e-4)
         @test dfw(θ, x0; fwkw...) ≈ sqrt.(vcat(θ, sum(θ))) rtol = 1.0e-4
         J = Zygote.jacobian(_θ -> dfw(_θ, x0; fwkw...), θ)[1]
         J_ref = Zygote.jacobian(_θ -> sqrt.(vcat(_θ, sum(_θ))), θ)[1]
